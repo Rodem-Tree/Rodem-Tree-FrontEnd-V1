8269057696bc84b57f91d8746995ef4453df0da1
임수빈 <hange253@gmail.com>
2022-11-16 23:50:43 +0900

    [DOC] MentalFood

---
+++ docs/mentalFoodDocs.tsx
@@ -0,0 +1,14 @@
+import { mentalFoodDocsType } from "utils/interface/mentalFoodType";
+
+export const mentalFoodDocs: mentalFoodDocsType[] = [
+  {
+    title: "지금 인기있는 시를 확인해보세요",
+    content: "명예의 전당 페이지에서 추천수 기준으로 시를 확인할 수 있습니다",
+    img: "/img/main/dailyFood1.png",
+  },
+  {
+    title: "다양한 종류의 시를 확인해보세요",
+    content: "로뎀나무에 등록된 모든 시를 확인할 수 있습니다",
+    img: "/img/main/dailyFood2.png",
+  },
+];
