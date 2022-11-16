738688db81c0b7c74261cd75eb101396073763a9
임수빈 <hange253@gmail.com>
2022-11-16 23:51:45 +0900

    [CREATE] Login

---
+++ pages/login.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+import Background from "components/login/Background";
+
+export default function Ligon() {
+  return (
+    <Container>
+      <Background />
+    </Container>
+  );
+}
+
+const Container = styled.div``;
